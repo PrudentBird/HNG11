5ac8f41d5256fb9f3379461a56e4182d57d5bfb1
PrudentBird <prudentbird@gmail.com>
2024-07-09 08:28:20 +0100

    Version History

--- Frontend/Stage_Two_Task/src/App.js
+++ Frontend/Stage_Two_Task/src/App.js
@@ -1,14 +1,13 @@
 import "./App.scss";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { LayoutProvider } from "./contexts/LayoutContext";
-import Home from "./pages/Home/Home";
 import Checkout from "./pages/Checkout/Checkout";
 import ProductListing from "./pages/ProductListing/ProductListing";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Home />,
+    element: <ProductListing />,
   },
   {
     path: "/checkout",
